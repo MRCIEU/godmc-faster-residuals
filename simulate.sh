7f0223af4801db1ce6553c37936eff522cc30862
Gibran Hemani <explodecomputer@gmail.com>
2025-03-19 23:35:19 +0000

    upd

--- simulate.sh
+++ simulate.sh
@@ -3,15 +3,17 @@
 # Simulate phenotype data
 
 bfile="/mnt/storage/private/alspacdata/datasets/dataset_gi_1000g_g0m_g1/released/2015-10-30/data/derived/filtered/bestguess/maf0.01_info0.8/combined/data"
+
 # Generate 100 for every sim below e.g. nsim=10 gives 1000 phenotypes
 nsim=10
 
 mkdir -p out/sim
 > out/cischr
 
+
 for i in $(seq 1 $nsim); do
-    # shuf -n 1000 $bfile.bim | awk '{ print $2, $5, rand() }' > out/sim/score$i.txt
-    # plink2 --bfile $bfile --score out/sim/score$i.txt --out out/sim/score$i
+    shuf -n 1000 $bfile.bim | awk '{ print $2, $5, rand() }' > out/sim/score$i.txt
+    plink2 --bfile $bfile --score out/sim/score$i.txt --out out/sim/score$i
     for j in {1..100}; do
         echo "5" >> out/cischr
     done
